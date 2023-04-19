8632773e033119115200ac1068da06f7dbe5b76a
BerkeAnna <berke.anna0803@gmail.com>
2023-04-19 10:19:59 +0200

    1

---
+++ Problem #1/Problem_PHP.php
@@ -0,0 +1,17 @@
+<?php
+
+class Problem_PHP
+{
+    public function numberList($numberArray, $k )
+    {
+        for($i=0; $i<sizeof($numberArray); $i++){
+            for($j=0; $j<sizeof($numberArray); $j++){
+                if($numberArray[$i] != $numberArray[$j] && ($numberArray[$i]+$numberArray[$j]) == $k){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+}
